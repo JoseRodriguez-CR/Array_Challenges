2badb1a32650a11b88a55f81af42fedfdfde37ec
JoseRodriguez-CR <josemariorodriguezc@gmail.com>
2021-09-17 18:19:39 -0600

    Updating File

--- Array_Challenges.js
+++ Array_Challenges.js
@@ -23,3 +23,31 @@ alwaysHungry([3.14, "food", "pie", true, "food"]);
 // this should console log "yummy", "yummy"
 alwaysHungry([4, 1, 5, 7, 2]);
 // this should console log "I'm hungry"
+
+console.log("--------End of challenges #1-------");
+
+// 2. High Pass Filter
+
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for( i = 0; i <= arr.length; i ++ ){
+        if( arr[i] > cutoff ){
+            //console.log(arr[i]);
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
+
+// 3. High Pass Filter
+
+
+
+
+
+
+
+
